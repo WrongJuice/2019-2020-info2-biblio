dc1eb892ce14c8786a4417e2425cf962f26fab71
Alfred Gaillard <alfred.gaillard.pro@gmail.com>
2020-01-21 12:08:58 +0100

    Modification de la twig afin de restructurer le HTML (plus propre en plus d'être nécessaire pour pouvoir faire fonctionner les test)

--- mpapws/tests/FormControllerTest.php
+++ mpapws/tests/FormControllerTest.php
@@ -5,17 +5,54 @@ namespace App\Tests;
 use App\Controller\FormController;
 use PHPUnit\Framework\TestCase;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class FormControllerTest extends WebTestCase
 {
 
-    public function testShowPost()
+    public function testPageIsSuccessful()
     {
         $client = static::createClient();
 
         $client->request('GET', '/formulaire');
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertTrue($client->getResponse()->isSuccessful());
+    }
+
+    public function testSubmitForm()
+    {
+        $client = static::createClient();
+        $client->followRedirects(true);
+        $crawler = $client->request('GET', '/formulaire');
+
+
+
+        $book = new UploadedFile(
+            'public/data_test/TP - TS3.pdf',
+            'TP - TS3.pdf',
+            'application/pdf',
+            null
+        );
+
+        //$form = $crawler->selectButton('Envoyer')->form();
+
+        //$form = $crawler->filter('.formUp')->form();
+        //$form['titre'] = 'BD_de_test';
+
+        $crawler = $client->submitForm('Envoyer',
+            ['titre' => 'BD_de_test',
+            'auteur' => 'Alfred',
+            'description' => 'Ceci est une BD de test',
+            'genre' => 'BD',
+            'sousGenre' => 'Aventure',
+            'LivrePDF' => $book]
+        );
+
+        //$form = $crawler->selectButton('save')->form();
+
+        // set some values
+        //$form['titre'] = 'BD_Test';
+
     }
 
 }
