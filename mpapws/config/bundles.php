2b7a39d6b32ddf182e9ef5f94e7e895a065bc0e2
Alfred Gaillard <alfred.gaillard.pro@gmail.com>
2020-01-21 12:27:34 +0100

    Fix oublie du texte de git

--- mpapws/config/bundles.php
+++ mpapws/config/bundles.php
@@ -7,16 +7,6 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-<<<<<<< HEAD
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['all' => true, 'BDTendancesTest' => true],
-=======
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['all' => true],
->>>>>>> 97cc96542de901476df18499dffea592290b2aa7
-    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
-    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'BDTendancesTest' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'BDTendancesTest' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true]
 ];
 
